21450754e98ed47960e5446c255dfb1573c2df0e
Isabela Nascimento <isabela.nascimento@nava.com.br>
2023-08-02 17:17:46 -0300

    feat: first commit

---
+++ src/main/java/com/example/backbbibm/BackBbIbmApplication.java
@@ -0,0 +1,13 @@
+package com.example.backbbibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackBbIbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackBbIbmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backbbibm/BackBbIbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backbbibm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackBbIbmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
